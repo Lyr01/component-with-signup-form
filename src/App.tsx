a9c2189a9707fd468b38aa97e71c8d22f38ee29f
Lyr01 <79974123+Lyr01@users.noreply.github.com>
2022-11-30 19:01:50 +0100

    completed for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,32 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			Learn to code by watching others See how experienced developers solve
-			problems in real-time. Watching scripted tutorials is great, but
-			understanding how developers think is invaluable. Try it free 7 days then
-			$20/mo. thereafter First Name Last Name Email Address Password Claim your
-			free trial By clicking the button, you are agreeing to our Terms and
-			Services
-			<footer>
+			<div className="left-section">
+				<h1 className="title">Learn to code by watching others</h1>
+				<p>
+					See how experienced developers solve problems in real-time.Watching
+					scripted tutorials is great, but understanding how developers think is
+					invaluable.
+				</p>
+			</div>
+			<div className="right-section">
+				<div className="free-trial">
+					<b>Try it free 7 days</b> then $20/mo.thereafter
+				</div>
+				<form>
+					<input type="text" placeholder="First Name" />
+					<input type="text" placeholder="Last Name" />
+					<input type="email" placeholder="Email Adress" />
+					<input type="passwoard" placeholder="Passwoard" />
+					<button className="btn-claim-trial">Claim your free trial</button>
+					<small className="term-services">
+						By clicking the button, you are agreeing to our{" "}
+						<b className="color-red">Terms and Services</b>
+					</small>
+				</form>
+			</div>
+
+			{/* <footer>
 				<p className="attribution">
 					Challenge by{" "}
 					<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
@@ -17,7 +36,7 @@ function App() {
 					</a>
 					. Coded by <a href="#">Your Name Here</a>.
 				</p>
-			</footer>
+			</footer> */}
 		</div>
 	);
 }
