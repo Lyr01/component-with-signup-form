e6e8546e1e436fd7ec43e87023dc6f98cbcb8891
Lyr01 <79974123+Lyr01@users.noreply.github.com>
2022-12-01 11:00:20 +0100

    complete typescript and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [passwoard, setPasswoard] = useState("");
+	const [showError, setShowError] = useState(false);
+	const [emailIsValid, setEmailIsValid] = useState(false);
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (!firstName || !lastName || !email || !passwoard) setShowError(true);
+
+		/*see if email is valid*/
+		if (!/\S+@\S+\.\S+/.test(email)) {
+			setEmailIsValid(true);
+		} else {
+			setEmailIsValid(false);
+		}
+	};
+
 	return (
 		<div className="App">
 			<div className="left-section">
@@ -15,28 +35,94 @@ function App() {
 				<div className="free-trial">
 					<b>Try it free 7 days</b> then $20/mo.thereafter
 				</div>
-				<form>
-					<input type="text" placeholder="First Name" />
-					<input type="text" placeholder="Last Name" />
-					<input type="email" placeholder="Email Adress" />
-					<input type="passwoard" placeholder="Passwoard" />
-					<button className="btn-claim-trial">Claim your free trial</button>
+				<form onSubmit={(e) => handleSubmit(e)}>
+					<input
+						className={!firstName && showError ? "input-error" : ""}
+						type="text"
+						placeholder="First Name"
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+					/>
+					<span
+						className="error"
+						style={{
+							display: !firstName && showError ? "block" : "none",
+						}}
+					>
+						First Name cannot be empty
+					</span>
+
+					<input
+						className={!lastName && showError ? "input-error" : ""}
+						type="text"
+						placeholder="Last Name"
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+					/>
+					<span
+						className="error"
+						style={{
+							display: !lastName && showError ? "block" : "none",
+						}}
+					>
+						Last Name cannot be empty
+					</span>
+
+					<input
+						className={
+							(!email || emailIsValid) && showError ? "input-error" : ""
+						}
+						type="text"
+						placeholder="Email Adress"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<span
+						className="error"
+						style={{
+							display: (!email || emailIsValid) && showError ? "block" : "none",
+						}}
+					>
+						Looks like this is not an email
+					</span>
+
+					<input
+						className={!passwoard && showError ? "input-error" : ""}
+						type="passwoard"
+						placeholder="Passwoard"
+						value={passwoard}
+						onChange={(e) => setPasswoard(e.target.value)}
+					/>
+					<span
+						className="error"
+						style={{
+							display: !passwoard && showError ? "block" : "none",
+						}}
+					>
+						Passwoard cannot be empty
+					</span>
+
+					<button type="submit" className="btn-claim-trial">
+						Claim your free trial
+					</button>
 					<small className="term-services">
 						By clicking the button, you are agreeing to our{" "}
 						<b className="color-red">Terms and Services</b>
 					</small>
 				</form>
+				<footer>
+					<p className="attribution">
+						Challenge by{" "}
+						<a
+							href="https://www.frontendmentor.io?ref=challenge"
+							target="_blank"
+						>
+							Frontend Mentor
+						</a>
+						. Coded by <a href="https://lyr01.github.io/">Hamza Khan</a>.
+					</p>
+				</footer>
 			</div>
-
-			{/* <footer>
-				<p className="attribution">
-					Challenge by{" "}
-					<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-						Frontend Mentor
-					</a>
-					. Coded by <a href="#">Your Name Here</a>.
-				</p>
-			</footer> */}
 		</div>
 	);
 }
